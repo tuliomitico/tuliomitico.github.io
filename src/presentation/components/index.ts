17c7699d4834788044adce4d27b6541b3f3262ec
tuliomitico <tulioneedforspeed@gmail.com>
2025-05-22 20:11:43 -0300

    feat: add theme provider by shadcn ui

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./contact-form/contact-form";
 export * from "./project-card/project-card";
 export * from "./tech-stack/tech-stack";
+export * from "./theme-toggle/theme-toggle";
 export * from "./ui";
 export * from "./logos";
